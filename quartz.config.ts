c5a655efec1bffada0766f4d8cd20fe108cb2cf2
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2026-03-08 11:12:06 -0700

    feat: rename site title to 'obenauer'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "obenauer.me",
+    pageTitle: "obenauer",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
